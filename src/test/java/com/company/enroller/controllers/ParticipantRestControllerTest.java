760bfa6dcb6e8d5fd6e3b8eabab658ebc70aac27
student19 <student19@pcoip.ki.agh.edu.pl>
2018-05-20 13:59:55 +0200

    important comments

--- src/test/java/com/company/enroller/controllers/ParticipantRestControllerTest.java
+++ src/test/java/com/company/enroller/controllers/ParticipantRestControllerTest.java
@@ -18,6 +18,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 import com.company.enroller.model.Participant;
 import com.company.enroller.persistence.MeetingService;
@@ -41,12 +42,60 @@ public class ParticipantRestControllerTest {
 		Participant participant = new Participant();
 		participant.setLogin("testlogin");
 		participant.setPassword("testpassword");
+		//mock bazy danych;
+				Collection<Participant> allParticipants = singletonList(participant);
+				given(participantService.getAll()).willReturn(allParticipants);
+				
+		//konfigurujemy serwis żeby na zapytanie getAll() zawsze odpowiadał listą allParticipants stworzoną linijkę wyżej
+				
+				mvc.perform(get("/participants").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+						.andExpect(jsonPath("$", hasSize(1))).andExpect(jsonPath("$[0].login", is(participant.getLogin())));
+		//taki sposob pisania api nazywa sie FLUENT API, pisane w jednej linijce
+			}
+			
+	@Test
+	public void getParticipant() throws Exception {
+		Participant participant = new Participant();
+		participant.setLogin("testlogin");
+		participant.setPassword("testpassword");
 
-		Collection<Participant> allParticipants = singletonList(participant);
-		given(participantService.getAll()).willReturn(allParticipants);
+		given(participantService.findByLogin(participant.getLogin())).willReturn(participant);
 
-		mvc.perform(get("/participants").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
-				.andExpect(jsonPath("$", hasSize(1))).andExpect(jsonPath("$[0].login", is(participant.getLogin())));
+		mvc.perform(get("/participants/testlogin").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(content().string("{\"login\":\"testlogin\",\"password\":\"testpassword\"}"))
+				.andExpect(jsonPath("login", is("testlogin")));
 	}
 
+	@Test
+	public void getNonExistringParticipant() throws Exception {
+			mvc.perform(get("/participants/anotherUser").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isNotFound())
+			.andExpect(content().string(""));
+	}
+
+	@Test
+	public void removeParticipant() throws Exception {
+		Participant participant = new Participant();
+		participant.setLogin("testlogin");
+		participant.setPassword("testpassword");
+		given(participantService.findByLogin(participant.getLogin())).willReturn(participant);
+		participantService.delete(participant);
+		mvc.perform(delete("/participants/testlogin").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isNoContent())
+				.andExpect(content().string(""));
+
+	}
+
+	@Test
+	public void addParticipant() throws Exception {
+		Participant participant = new Participant();
+		participant.setLogin("testlogin");
+		participant.setPassword("testpassword");
+
+		given(participantService.add(participant)).willReturn(participant);
+
+		mvc.perform(get("/participants/testlogin").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(content().string("{\"login\":\"testlogin\",\"password\":\"testpassword\"}"))
+				.andExpect(jsonPath("login", is("testlogin")));
+	}
+
+//kontrolery obejmuja logike biznesowa: odpowiadaja za np. zlozenie faktury, naliczenie rabatu itp.
 }
